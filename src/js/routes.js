901505a689a8ecb51318d673fbda26f0a572c6aa
kimia <shzkimia@gmail.com>
2019-11-12 12:36:52 +0330

    first routing has set

---
+++ src/js/routes.js
@@ -0,0 +1,6 @@
+export default {
+    '/': 'Home',
+    '/about': 'About',
+    '/kimia': 'Kimia'
+  }
+  
--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
-import style from './main.sass'
+import style from './sass/main.sass'
+import Vue from 'vue'
+import routes from './js/routes'
+
+const app = new Vue({
+    el: '#app',
+    data: {
+      currentRoute: window.location.pathname
+    },
+    computed: {
+      ViewComponent () {
+        const matchingView = routes[this.currentRoute];
+        return require('./js/pages/' + (matchingView || '404') + '.vue')
+      }
+    },
+    render (h) {
+      return h(this.ViewComponent)
+    }
+});
